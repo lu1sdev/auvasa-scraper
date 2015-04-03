55846b11894697b4667c351b3caa0e3f096664e8
Luis de Dios Martín <luisddm@gmail.com>
2015-04-03 20:55:46 +0200

    Added main

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ var iconv = require('iconv-lite');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('main', { title: 'Express' });
 });
 
 router.get('/parada/:num', function(req, res, next) {
