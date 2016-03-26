22b78d0bf7bb84d0ff893846ccc25a4bf52fbdba
Luis de Dios Martín <luisddm@gmail.com>
2016-03-26 11:34:04 +0100

    Code cleanup & ES6 features

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 /*jslint node: true */
-"use strict";
+'use strict';
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const routes = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,19 +19,19 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(require('node-compass')({mode: 'expanded'}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(require('node-compass')({mode: 'expanded'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -41,24 +41,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
   });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,59 @@
 /*jslint node: true */
-"use strict";
+'use strict';
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var cheerio = require('cheerio');
-var request = require('request');
+const cheerio = require('cheerio');
+const request = require('request');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('main', { title: 'Auvasa Scraper' });
 });
 
-router.get('/parada/:num', function(req, res, next) {
+router.get('/parada/:num', (req, res, next) => {
 
-	var url = 'http://www.auvasa.es/paradamb.asp?codigo=' + req.params.num;
+  const url = 'http://www.auvasa.es/paradamb.asp?codigo=' + req.params.num;
 
-	// The structure of our request call
-	// The first parameter is our URL
-	// The callback function takes 3 parameters, an error, response status code and the html
+  // The structure of our request call
+  // The first parameter is our URL
+  // The callback function takes 3 parameters, an error, response status code and the html
 
-	request(url, function(error, response, html) {
+  request(url, (error, response, html) => {
 
-		// First we'll check to make sure no errors occurred when making the request
+    // First we'll check to make sure no errors occurred when making the request
 
-		if(!error) {
+    if (!error) {
+      // Next, we'll utilize the cheerio library on the returned html which will essentially
+      // give us jQuery functionality
+      const $ = cheerio.load(html);
 
-			// Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-			var $ = cheerio.load(html);
+      // Get the name of the bus line
+      const title = $('#mainleft0').find('h1').eq(0).text();
 
-			// Get the name of the bus line
-			var title = $('#mainleft0').find("h1").eq(0).text();
+      // Get the timetable of the next buses
+      const horarios = [];
 
-			// Get the timetable of the next buses
-			var horarios = [];
+      $('.style36').each((i, element) => {
+        const $td = $(this).find('.style38');
 
-			$(".style36").each(function(i, element) {
-				var $td = $(this).find(".style38");
+        const linea = $td.eq(0).text();
+        const dest = $td.eq(2).text();
+        const tiempo = $td.eq(3).text();
 
-				var linea = $td.eq(0).text();
-				var dest = $td.eq(2).text();
-				var tiempo = $td.eq(3).text();
+        const ruta = { linea: linea, dest: dest, tiempo: tiempo };
 
-				var ruta = { linea: linea, dest: dest, tiempo: tiempo };
+        horarios.push(ruta);
+      });
 
-				horarios.push(ruta);
-			});
+      res.render('index', { title: title, horarios: horarios });
 
-			res.render('index', { title: title, horarios: horarios });
+    } else {
+      res.render('error', { message: 'Error', error: error });
+    }
 
-		} else {
-			res.render('error', { message: "Error", error: error });
-		}
-
-	});
+  });
 
 });
 
