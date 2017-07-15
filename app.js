b9351301715600a64dc5dcb2b0859f865dac823c
Luis de Dios <luis.dedios@barrabesmeaning.com>
2017-07-15 22:24:43 +0200

    Add Rest API

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-/*jslint node: true */
-'use strict';
-
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
@@ -9,17 +6,17 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const routes = require('./routes/index');
-const users = require('./routes/users');
 
 const app = express();
+
+app.disable('x-powered-by');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-//app.use(require('node-compass')({mode: 'expanded'}));
+// app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,80 @@
-/*jslint node: true */
-'use strict';
-
 const express = require('express');
-const router = express.Router();
-
 const cheerio = require('cheerio');
 const request = require('request');
+
+const router = express.Router();
+
+const host = 'http://www.auvasa.es';
+const namespace = 'parada.asp';
+
+function getTimetable(html, id) {
+  // Next, we'll utilize the cheerio library on the returned html which will essentially
+  // give us jQuery functionality
+  const $ = cheerio.load(html, { decodeEntities: false });
+
+  // Get the name of the bus stop
+  const name = $('.col_three_fifth').find('h5').eq(0).text()
+    .replace(/\s+/g, ' ')
+    .trim();
+
+  // Get the timetable of the next buses
+  const timetable = [];
+
+  $('.table-hover tbody').each((i, element) => {
+    const $td = $(element).find('td');
+
+    const line = $td.eq(0).text();
+    const prm = Boolean($td.eq(1).children().length);
+    const destination = $td.eq(3).text();
+    const time = Number($td.eq(4).text());
+
+    const route = { line, prm, destination, time };
+
+    timetable.push(route);
+  });
+
+  return { id, name, timetable };
+}
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('main', { title: 'Auvasa Scraper' });
 });
 
-router.get('/parada/:num', (req, res, next) => {
-
-  const url = 'http://www.auvasa.es/parada.asp?codigo=' + req.params.num;
+router.get('/web/stop/:id', (req, res, next) => {
+  const uri = `${host}/${namespace}?codigo=${req.params.id}`;
 
   // The structure of our request call
   // The first parameter is our URL
   // The callback function takes 3 parameters, an error, response status code and the html
 
-  request(url, (error, response, html) => {
-
+  request({ uri, encoding: 'binary' }, (error, response, html) => {
     // First we'll check to make sure no errors occurred when making the request
 
     if (!error) {
-      // Next, we'll utilize the cheerio library on the returned html which will essentially
-      // give us jQuery functionality
-      const $ = cheerio.load(html);
-
-      // Get the name of the bus stop
-      const title = $('.col_three_fifth').find('h5').eq(0).text();
-
-      // Get the timetable of the next buses
-      const horarios = [];
-
-      $('.table-hover tbody').each((i, element) => {
-        const $td = $(element).find('td');
-
-        const linea = $td.eq(0).text();
-        const dest = $td.eq(3).text();
-        const tiempo = $td.eq(4).text();
-
-        const ruta = { linea: linea, dest: dest, tiempo: tiempo };
-
-        horarios.push(ruta);
-      });
-
-      res.render('index', { title: title, horarios: horarios });
-
+      res.render('index', getTimetable(html, +req.params.id));
     } else {
-      res.render('error', { message: 'Error', error: error });
+      res.render('error', { message: 'Error', error });
     }
-
   });
+});
 
+router.get('/api/stop/:id', (req, res, next) => {
+  const uri = `${host}/${namespace}?codigo=${req.params.id}`;
+
+  // The structure of our request call
+  // The first parameter is our URL
+  // The callback function takes 3 parameters, an error, response status code and the html
+
+  request({ uri, encoding: 'binary' }, (error, response, html) => {
+    // First we'll check to make sure no errors occurred when making the request
+
+    if (!error) {
+      res.json(getTimetable(html, +req.params.id));
+    } else {
+      res.json({ error });
+    }
+  });
 });
 
 module.exports = router;
